23d4cd30139b9748afdf9b325184ce9f8d1d0162
LuisFeQ <85293686+Luisgato21@users.noreply.github.com>
2024-09-14 00:03:48 -0300

    Jv

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+// Exemplo: animação suave para rolagem ao clicar nos links do menu
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
