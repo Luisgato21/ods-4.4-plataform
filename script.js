390818072d3b3a9491a3aae992a9c69ca4644a98
LuisFeQ <fileps2009@gmail.com>
2024-09-18 14:21:14 -0300

    Remoção de comentarios

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// script.js
-
-// Exemplo: animação suave para rolagem ao clicar nos links do menu
 document.querySelectorAll('.nav-link').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
